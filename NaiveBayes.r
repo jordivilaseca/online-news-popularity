bd3b675adc63f6f5d00f42c2d7bafe93184f2367
Jordi Vilaseca <jordi.vilaseca.corderroure@est.fib.upc.edu>
2016-01-02 08:34:02 +0100

    Small changes on NaiveBayes.r

--- NaiveBayes.r
+++ NaiveBayes.r
@@ -5,7 +5,10 @@
 library (e1071)
 
 # Read data from csv
-data <- read.csv(file="./OnlineNewsPopularity/OnlineNewsPopularity.csv", header=TRUE, sep=",")
+data <- read.csv(file="./OnlineNewsPopularity.csv", header=TRUE, sep=",")
+
+# Remove url from dataset, it does not give the model any information.
+data$url <- NULL
 
 summary(data)
 
@@ -15,8 +18,8 @@ THRESHOLD <- 1400
 
 # Transform the shares variable from a continuous to a discrete one using the threshold
 # and mark it as a factor.
-data$shares[data$shares <= THRESHOLD] = 0
-data$shares[data$shares > THRESHOLD] = 1
+data$shares[data$shares < THRESHOLD] = 0
+data$shares[data$shares >= THRESHOLD] = 1
 data$shares <- factor(data$shares, labels = c("unpopular","popular"))
 
 summary(data$shares)
@@ -33,14 +36,8 @@ ntest <- N - nlearn
 # Train the model
 model <- naiveBayes(shares ~ ., data = data[learn,])
 
-# Predict, get the veredict for the first 20 rows
-predict(model, data[1:20,-61]) 
-
-# Predict, get the probabilities for the first 20 rows
-predict(model, data[1:20,-61], type = "raw")
-
 # compute now the apparent error for the learn set
-pred <- predict(model, data[learn,-61])
+pred <- predict(model, data[learn,-60])
 
 # form and display confusion matrix & overall error
 tab <- table(pred, data[learn,]$shares) 
@@ -48,19 +45,22 @@ tab
 1 - sum(tab[row(tab)==col(tab)])/sum(tab)
 
 # compute the test (prediction) error
-pred <- predict(model, newdata=data[-learn,-61])
+pred <- predict(model, newdata=data[-learn,-60])
 
 # form and display confusion matrix & overall error
 tab <- table(pred, data[-learn,]$shares) 
 tab
 1 - sum(tab[row(tab)==col(tab)])/sum(tab)
 
-model <- naiveBayes(shares ~ ., data = data[learn,], laplace = 1)
-
-# compute the test (prediction) error
-pred <- predict(model, newdata=data[-learn,-61])
-
-# form and display confusion matrix & overall error
-tab <- table(pred, data[-learn,]$shares) 
-tab
-1 - sum(tab[row(tab)==col(tab)])/sum(tab)
+for (i in seq(0,3,0.25)) {
+  model <- naiveBayes(shares ~ ., data = data[learn,], laplace = i)
+   
+  # compute the test (prediction) error
+  pred <- predict(model, newdata=data[-learn,-60])
+  
+  print(paste("Laplace parameter equals",toString(i)))
+  # form and display confusion matrix & overall error
+  tab <- table(pred, data[-learn,]$shares) 
+  print(tab)
+  print(1 - sum(tab[row(tab)==col(tab)])/sum(tab)) 
+}
